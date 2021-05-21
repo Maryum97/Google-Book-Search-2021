1508c7dfcb3f66621325beb17b6db94828d15a65
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-21 18:45:39 +0100

    added function to render book info

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -20,27 +20,32 @@ function SearchBooks() {
     // declare function to load books
     const searchBooks = (query) => {
         API.getBook(query)
-        .then(books => {
-            setBook(book);
-            setBooks(books);
-        })
-        .catch(err => console.log('error message: ' + err));
+            .then(books => {
+                setBook(book);
+                setBooks(books);
+            })
+            .catch(err => console.log('error message: ' + err));
     }
 
     // declare function to get search results onto page
-    function getSearchResluts() {
-        
+    function getSearchResults(bookData) {
+        return {
+            title: bookData.title,
+            author: bookData.author,
+            synopsis: bookData.synopsis,
+            date: bookData.date
+        }
     }
 
     // declare function for event of change in form input
-    function handleInputChange (e) {
+    function handleInputChange(e) {
         setSearch(e.target.value);
     }
 
     // declare function for event of clicking on submit button
-    function handleFormSubmit (e) {
+    function handleFormSubmit(e) {
         e.preventDefault();
-        getSearchResluts(search);
+        getSearchResults(search);
     }
 
     // clear search
@@ -53,17 +58,21 @@ function SearchBooks() {
     return (
         <div>
             <Jumbotron
-                    heading='Google Books Search'
-                    text='Search for your favourite books right here on this page :)'
-                />
-                <br></br>
+                heading='Google Books Search'
+                text='Search for your favourite books right here on this page :)'
+            />
+            <br></br>
             <Container>
-                <FormBooks 
+                <FormBooks
                     value={search}
                     handleInputChange={handleInputChange}
                     handleFormSubmit={handleFormSubmit}
                     clearSearch={clearSearch}
                 />
+                <br></br>
+                <Row>
+                    <h2>Results</h2>
+                </Row>
             </Container>
         </div>
     )
