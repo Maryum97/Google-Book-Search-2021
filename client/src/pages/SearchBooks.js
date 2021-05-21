76006e962e43b0ea0b9b52f53b5204595f200716
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-21 17:56:11 +0100

    added JSX code

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,10 +1,31 @@
-import React from 'react';
-
-import Jumbotron from '../components/Jumbotron';
-
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
+// import components
+import Jumbotron from '../components/Jumbotron';
+import API from '../utils/API';
+
 function SearchBooks() {
+    // declare state variables
+    const [book, setBook] = useState({});
+    const [books, setBooks] = useState([]);
+    // const [search, setSearch] = useState('');
+
+    // when the component mounts, make call to get books
+    useEffect(() => {
+        searchBooks();
+    }, []);
+
+    // declare function to load books
+    const searchBooks = (query) => {
+        API.getBook(query)
+        .then(books => {
+            setBook(book);
+            setBooks(books);
+        })
+        .catch(err => console.log('error message: ' + err));
+    }
+    
     return (
         <div>
             <Jumbotron
