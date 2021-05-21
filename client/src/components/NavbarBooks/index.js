459c54f10bb52566761e120fb93c7a4e2f9df88d
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-21 12:19:49 +0100

    included new pages and components

---
+++ client/src/components/NavbarBooks/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+
+function NavbarBooks () {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar color="info" light expand="md">
+                <NavbarBrand href="/"><b>Reactstrap</b>BookSearch</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/searchbooks">Search For Books</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/savedbooks">Saved Books</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    );
+}
+
+export default NavbarBooks;
