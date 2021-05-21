e1369012f88faa6b6c2f1fc19ae2a1eae6b42cca
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-21 16:27:52 +0100

    added component

---
+++ client/src/components/JumbotronBooks/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Jumbotron } from 'reactstrap';
+
+function JumbotronBooks({ props }) {
+    return (
+        <div>
+            <Jumbotron>
+                <h1>{props.heading}</h1>
+                <p>{props.text}</p>
+            </Jumbotron>
+        </div>
+    )
+}
+
+export default JumbotronBooks;
