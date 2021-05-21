8b0f384b7cccf41a685f6be59d44401b1cdfc091
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-21 12:17:02 +0100

    added pages under switch condition

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import SavedBooks from './pages/SavedBooks';
+import SearchBooks from './pages/SearchBooks';
+import NoMatch from './pages/NoMatch';
+
+import NavbarBooks from './components/NavbarBooks';
 import Footer from './components/Footer';
-import Books from './pages/Books';
 
 function App () {
   return (
-    <div>
-      <Navbar />
-      <br></br>
-      <Books />
-      <Footer />
+    <Router>
+      <div>
+        <NavbarBooks />
+        <Switch>
+
+          <Route exact path='/searchbooks' component={SearchBooks}></Route>
+
+          <Route exact path='/savedbooks' conponent={SavedBooks}></Route>
+
+          <Route>
+            <NoMatch />
+          </Route>
+
+        </Switch>
+        <Footer />
       </div>
+    </Router>
   )
 }
 
