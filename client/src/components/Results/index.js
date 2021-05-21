78e604bf214a489f56e7bd7dafe55ca6905846bb
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-21 19:04:34 +0100

    added card to display book info

--- client/src/components/Results/index.js
+++ client/src/components/Results/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {
+    Card, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 import API from '../../utils/API';
 
 function Results() {
     return (
         <div>
-            Hello, I am your search results!
+            <Card>
+                <CardBody>
+                    <CardTitle tag="h5">Book title</CardTitle>
+                    <CardSubtitle tag="h6" className="mb-2 text-muted">Date</CardSubtitle>
+                    <CardText>Synopsis</CardText>
+                    <Button>Save</Button>
+                </CardBody>
+            </Card>
         </div>
     )
 }
