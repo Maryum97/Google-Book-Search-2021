6f4e2292286b89c72732847be9821c59c1bd154c
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-21 18:57:31 +0100

    added results component

---
+++ client/src/components/Results/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import API from '../../utils/API';
+
+function Results() {
+    return (
+        <div>
+            Hello, I am your search results!
+        </div>
+    )
+}
+
+export default Results;
