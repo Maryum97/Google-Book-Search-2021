aa7599d09b78b296a0e578f13e17175ac02abec3
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-22 14:45:19 +0100

    enabled page to render saved books

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import API from '../utils/API';
 
 // import components
 import Jumbotron from '../components/Jumbotron';
+import Results from '../components/Results';
 
 function SavedBooks() {
+    // declare state variables
+    const [savedBooks, setSavedBooks] = useState([]);
+
+    // when the component mounts, make call to get saved books
+    useEffect(() => {
+        renderSavedBooks();
+    });
+
+    // declare function to render saved books
+    function renderSavedBooks() {
+        API.savedBooks()
+            .then(savedBooks => {
+                setSavedBooks(savedBooks)
+            })
+            .catch(err => console.log('error message: ' + err));
+    }
+
     return (
         <div>
             <Jumbotron
                 heading='Google Books Search'
                 text='View your saved books here'
             />
+            <br></br>
             <Container>
-                Saved Books Here!
+                <h2>Saved Books</h2>
+                <Results books={savedBooks}/>
             </Container>
         </div>
     )
