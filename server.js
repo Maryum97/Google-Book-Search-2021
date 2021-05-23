b48d80d31e3cbad8b259e25ce1842f966cd7a7bb
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-23 06:46:40 +0100

    amended code here

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/index.js');
+const db = mongoose.connection;
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -10,14 +11,16 @@ app.use(express.json());
 
 // Serves static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname + '/client/build'))
+  app.use(express.static('/client/build'))
 }
 
 // API routes
 app.use(routes);
 
-// Mongo DB Connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+db.on("Error on Mongo connection", error => console.error(error));
+db.once("connected", () => console.log("Success! You are connected to Mongoose"));
 
 app.listen(PORT, () => {
   console.log(`API server now on port:${PORT}`);
