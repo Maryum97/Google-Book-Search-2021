c5ef07f5cc4b51f93567c5e30ef5f17c30925a61
Maryum Bokhari <maryumbokhari97@gmail.com>
2021-05-23 13:21:14 +0100

    added new changes

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -2,56 +2,51 @@ const db = require("../models");
 
 // Defining methods for the booksController
 module.exports = {
-    findAll: function (req, res) {
-        db.Book
-            .find(req.query)
-            .sort({ date: -1 })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => {
-                console.log('Error:', err);
-                res.status(422).json(err)
-            });
-    },
-    findById: function (req, res) {
-        db.Book
-        db.Book
-            .findById(req.params.id)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => {
-                console.log('Error:', err);
-                res.status(422).json(err)
-            });
-    },
-    create: function (req, res) {
-        console.log('creating book...');
-        console.log(req.body);
-        db.Book
-            .create(req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => {
-                console.log('Error:', err);
-                res.status(422).json(err)
-            });
-    },
-    update: function (req, res) {
-        console.log('updating book...')
-        db.Book
-            .findOneAndUpdate({ _id: req.params.id }, req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => {
-                console.log('Error:', err);
-                res.status(422).json(err)
-            });
-    },
-    remove: function (req, res) {
-        console.log('deleting book...');
-        db.Book
-            .findById({ _id: req.params.id })
-            .then(dbModel => dbModel.remove())
-            .then(dbModel => res.json(dbModel))
-            .catch(err => {
-                console.log('Error:', err);
-                res.status(422).json(err)
-            });
-    }
+  findAll: function(req, res) {
+    db.Book
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => {
+        console.error(err)
+        res.status(422).json(err)
+      });
+  },
+  findById: function(req, res) {
+    db.Book
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => {
+        console.error(err)
+        res.status(422).json(err)
+      });
+  },
+  create: function(req, res) {
+    db.Book
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => {
+        console.error(err)
+        res.status(422).json(err)
+      });
+  },
+  update: function(req, res) {
+    db.Book
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => {
+        console.error(err)
+        res.status(422).json(err)
+      });
+  },
+  remove: function(req, res) {
+    db.Book
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => {
+        console.error(err)
+        res.status(422).json(err)
+      });
+  }
 };
